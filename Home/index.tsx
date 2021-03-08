6a2dca75a10d89db1052e9c1e28a806fb2f4d49d
nyanji00 <wl77886@gmail.com>
2021-03-08 21:20:26 +0900

    feat: Move to React App

--- Home/index.tsx
+++
@@ -1,4 +0,0 @@
-export { default as AdditionalHeader } from './component/AdditionalHeader'
-export { default as FixedHeader } from './component/FixedHeader'
-export { default as SidebarContainer } from './component/SidebarContainer'
-export { default as MainContainer } from './component/MainContainer'
