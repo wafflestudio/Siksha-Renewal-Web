0aad6945b3396f46fdd0f4f288912004ba3fa7ca
nyanji00 <wl77886@gmail.com>
2021-08-16 00:45:23 +0900

    og:image 추가

--- pages/_app.js
+++ pages/_app.js
@@ -9,6 +9,7 @@ function MyApp({ Component, pageProps }) {
                 <meta charSet="utf-8"/>
                 <link rel="icon" href="/favicon.ico"/>
                 <meta name="viewport" content="initial-scale=1.0, width=device-width"/>
+                <meta hid="og:image" property="og:image" content="https://siksha.wafflestudio.com/img/og-image.png" />
                 {/* eslint-disable-next-line @next/next/no-sync-scripts */}
                 <script type="text/javascript"
                         src="//dapi.kakao.com/v2/maps/sdk.js?appkey=97c9bf23c521ad7495139a70e395b4eb"/>
