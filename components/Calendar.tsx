21c6f0af4837c48b5ec1cfc35b654a3937c150e5
TheoLee72 <theolee72@naver.com>
2023-08-22 16:09:32 +0900

    feat: yesterday & tomorrow

--- components/Calendar.tsx
+++ components/Calendar.tsx
@@ -1,14 +1,16 @@
 import styled from "styled-components";
 import { useDispatchContext, useStateContext } from "../hooks/ContextProvider";
-import { formatDate, formatMonth, formatWeekday } from "../utils/FormatUtil";
+import { formatDate, formatWeekday } from "../utils/FormatUtil";
 import ReactCalendar from "react-calendar";
-import { useCallback, useEffect } from "react";
+import { useCallback, useEffect, useState } from "react";
 
 export default function Calendar() {
   const state = useStateContext();
   const dispatch = useDispatchContext();
 
   const { date, today } = state;
+  const [yesterday, setYesterday] = useState(new Date(date.valueOf() - 86400000));
+  const [tomorrow, setTomorrow] = useState(new Date(date.valueOf() + 86400000));
   const setDate = useCallback((date) => dispatch({ type: "SET_DATE", date: date }), [dispatch]);
 
   const toggleShowCal = () => dispatch({ type: "TOGGLE_SHOWCAL" });
@@ -24,13 +26,28 @@ export default function Calendar() {
     const current = new Date();
     setDate(current);
   }, [setDate]);
+  useEffect(() => {
+    yesterday.setFullYear(date.getFullYear());
+    tomorrow.setFullYear(date.getFullYear());
+    yesterday.setDate(1); // date initialize
+    tomorrow.setDate(1);
+    yesterday.setMonth(date.getMonth());
+    tomorrow.setMonth(date.getMonth());
+    yesterday.setDate(date.getDate() - 1);
+    tomorrow.setDate(date.getDate() + 1);
+  }, [date]);
 
   return (
     <>
       <DesktopContainer>
+        <DateNavi>
+          {formatDate(yesterday)}
+          {formatDate(date)}
+          {formatDate(tomorrow)}
+        </DateNavi>
         <DateText>{formatDate(date)}</DateText>
         <ReactCalendar
-          onChange={(day) => setDate(day)}
+          onChange={(day: Date) => setDate(day)}
           onActiveStartDateChange={({ activeStartDate }) => setDate(activeStartDate)}
           defaultActiveStartDate={today}
           value={date}
@@ -101,6 +118,23 @@ const MobileContainer = styled.div`
 
 const Arrow = styled.img`
   cursor: pointer;
+`;
+
+const DateNavi = styled.div`
+  font-weight: 700;
+  font-size: 15px;
+  line-height: 17px;
+  color: black;
+  position: absolute;
+  top: -30px;
+  left: 1000px;
+  white-space: nowrap;
+
+  @media (max-width: 768px) {
+    color: #fe8c59;
+    cursor: pointer;
+    top: 4px;
+  }
 `;
 
 const DateText = styled.div`
