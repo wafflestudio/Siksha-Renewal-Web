8d32013cee30a072e8048b062feca78d962ce7d9
테스트 <teseuteu@teseuteuui-MacBookPro.local>
2023-08-26 21:41:33 +0900

    feat: add rating strts component

---
+++ components/MenuDetail/Stars.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import styled from "styled-components";
+
+export default function Stars({ score }: { score: number }) {
+  return (
+    <StarContainer>
+      {Array.from(Array(5).keys()).map((i) => {
+        if (score >= i + 1) {
+          return <Star src="/img/star.svg" key={i} />;
+        } else if (score >= i + 0.5) {
+          return <Star src="/img/star.svg" key={i} />;
+        } else {
+          return <Star src="/img/star-empty.svg" key={i} />;
+        }
+      })}
+    </StarContainer>
+  );
+}
+
+const StarContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  width: 100%;
+  gap: 1px;
+`;
+
+const Star = styled.img`
+  width: 12px;
+  height: 12px;
+  fill: #ffd600;
+`;
