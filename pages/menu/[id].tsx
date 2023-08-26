1e06be096f5b405cc83fad5668703d591ed5ef59
테스트 <teseuteu@teseuteuui-MacBookPro.local>
2023-08-26 22:24:57 +0900

    feat: 메뉴 상세보가 ui 중 리뷰부분

--- pages/menu/[id].tsx
+++ pages/menu/[id].tsx
@@ -1,8 +1,10 @@
-import Header from "../../components/Header";
 import styled from "styled-components";
-import React, { use, useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useRouter } from "next/router";
 import axios from "axios";
+import APIendpoint from "../../constants/constants";
+import Header from "../../components/Header";
+import { ReviewItem } from "../../components/MenuDetail/ReviewItem.";
 
 interface MenuType {
   id: number;
@@ -16,15 +18,15 @@ interface MenuType {
   etc: string[];
   created_at: string;
   updated_at: string;
-  score: number;
+  score: number | null;
   review_cnt: number;
 }
 
-interface ReviewType {
+export interface ReviewType {
   id: number;
   menu_id: number;
   user_id: number;
-  score: number;
+  score: number | null;
   comment: string;
   etc: {};
   created_at: string;
@@ -46,11 +48,9 @@ export default function Menu() {
     async function fetchMenu() {
       setLoading(true);
       try {
-        const res = await axios
-          .get(`https://siksha-api.wafflestudio.com/menus/${id}`)
-          .then((res) => {
-            setMenu(res.data);
-          });
+        const res = await axios.get(`${APIendpoint()}/menus/plain/${id}`).then((res) => {
+          setMenu(res.data);
+        });
       } catch (e) {
         console.log(e);
         router.push("/");
@@ -61,7 +61,7 @@ export default function Menu() {
       setLoading(true);
       try {
         const res = await axios
-          .get(`https://siksha-api.wafflestudio.com/reviews/?menu_id=${id}&page=1&per_page=5`)
+          .get(`${APIendpoint()}/reviews/?menu_id=${id}&page=1&per_page=5`)
           .then((res) => {
             setReviews(res.data.result);
           });
@@ -76,25 +76,23 @@ export default function Menu() {
     });
   }, [id, setLoading]);
 
-  console.log(isLoading, menu, reviews);
-
   return (
     <>
+      <Header />
       {!isLoading && !!menu && (
         <Info>
-          <div>
-            {menu.name_kr}
-
+          <MenuContainer>
+            <div>{menu.name_kr}</div>
+          </MenuContainer>
+          <ReviewContainer>
+            <ReviewHeader>
+              <ReviewTitle>리뷰</ReviewTitle>
+              <ReviewPostButton>나의 평가 남기기</ReviewPostButton>
+            </ReviewHeader>
             {reviews.map((review) => (
-              <div key={review.id}>
-                {review.comment} | {review.score}
-              </div>
+              <ReviewItem key={review.id} review={review} />
             ))}
-          </div>
-          <div>
-            <div>평점: {menu.score.toFixed(1)}</div>
-            <div>리뷰 수: {menu.review_cnt}</div>
-          </div>
+          </ReviewContainer>
         </Info>
       )}
     </>
@@ -103,25 +101,51 @@ export default function Menu() {
 
 const Info = styled.div`
   display: flex;
-  z-index: 100;
-  color: "#FF9522";
 `;
 
-const DesktopContainer = styled.div`
+const MenuContainer = styled.div`
   display: flex;
-  justify-content: center;
-
-  @media (max-width: 768px) {
-    display: none;
-  }
 `;
 
-const MobileContainer = styled.div`
-  display: none;
+const ReviewContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 735px;
+  right: 0;
+  position: absolute;
+  border-left: 1px solid #eeeeee;
+  padding-left: 50px;
+  padding-right: 150px;
+  padding-top: 50px;
+  height: 100%;
+`;
 
-  @media (max-width: 768px) {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-  }
+const ReviewTitle = styled.div`
+  font-size: 24px;
+  font-weight: 400;
+`;
+
+const ReviewHeader = styled.div`
+  display: flex;
+  justify-content: space-between;
+  width: 100%;
+  padding: 0 24px 0 24px;
+  height: 48px;
+  align-items: center;
+`;
+
+const ReviewPostButton = styled.button`
+  background: #ff9522;
+  border-radius: 16px;
+  width: 138px;
+  height: 32px;
+  color: white;
+  font-size: 14px;
+  font-weight: 800;
+  text-align: center;
+  vertical-align: middle;
+  line-height: 32px;
+  border: none;
+  cursor: pointer;
 `;
