dd611df0b8b7745f6e0fddee5ca6b8c98ddd9cb1
TheoLee72 <theolee72@naver.com>
2023-09-11 10:40:40 +0900

    fix: env setting

--- constants/constants.ts
+++ constants/constants.ts
@@ -1,6 +1,5 @@
 export default function APIendpoint() {
-  console.log(process.env.NODE_ENV);
-  if (process.env.NODE_ENV === "production") {
+  if (process.env.NEXT_PUBLIC_NODE_ENV === "production") {
     return `https://siksha-api.wafflestudio.com`;
   }
   return `https://siksha-api-dev.wafflestudio.com`;
