a1f7f348d2f0d36560660da618af396764d8116f
yblee2001 <yblee2001@gmail.com>
2023-09-24 18:39:52 +0900

    context provider types

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,7 +1,34 @@
-export {};
-
-declare global {
+declare module types {
   interface Window {
     kakao: unknown;
   }
+
+  interface Data {
+      count: number
+      result: Array<any>
+  }
+
+  interface State {
+      date: Date
+      meal: string
+      data: Data
+      today: Date
+      showCal: boolean
+      showInfo: boolean
+      loading: boolean
+      infoData: any
+      loginStatus: boolean
+  }
+
+  type Action =
+      { type: "SET_DATE", date: Date } |
+      { type: "SET_MEAL", meal: string } |
+      { type: "SET_DATA", data: Data } |
+      { type: "SET_LOADING", loading: boolean } |
+      { type: "SET_INFODATA", infoData: any } |
+      { type: "TOGGLE_SHOWCAL", showCal: boolean } |
+      { type: "TOGGLE_SHOWINFO", showInfo: boolean } |
+      { type: "SET_LOGINSTATUS", loginStatus: boolean }
 }
+
+export default types
