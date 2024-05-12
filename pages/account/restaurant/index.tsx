884689f379630de73b5e3ad857ad0c6fd8358eaf
MinnieMinwoo <nhs075241@gmail.com>
2024-05-12 18:54:43 +0900

    feat: add routing folder

--- pages/account/restaurant/index.tsx
+++ pages/account/restaurant/index.tsx
@@ -1,29 +1,7 @@
-import axios from "axios";
-import Header from "../../components/Header";
-import ContextProvider from "../../hooks/ContextProvider";
-import { GlobalStyle } from "../../styles/globalstyle";
-import APIendpoint from "../../constants/constants";
-
-export default function Account() {
-  const access_token = localStorage.getItem("access_token");
-  console.log(`${APIendpoint()}/auth/me`);
-  axios
-    .get(`${APIendpoint()}/auth/me`, {
-      headers: { "authorization-token": `Bearer ${access_token}` },
-    })
-    .then((res) => {
-      console.log(res.data);
-    })
-    .catch((err) => {
-      console.log(err);
-    });
+export default function Setting_NonFavorite() {
   return (
     <>
-      <GlobalStyle />
-      <ContextProvider>
-        <Header />
-      </ContextProvider>
-      <div>account</div>
+      <div>order</div>
     </>
   );
 }
