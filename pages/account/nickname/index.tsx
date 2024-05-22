c76bb278b50669dc01c6a94f192d6ef2699bcaa6
Yumin22224 <yumin224@snu.ac.kr>
2024-05-22 20:26:20 +0900

    feat: add state (SET_USERINFO)

--- pages/account/nickname/index.tsx
+++ pages/account/nickname/index.tsx
@@ -1,7 +1,39 @@
-export default function Setting_Nickname() {
+import AccountLayout from "../accountLayout";
+import styled from "styled-components";
+import axios from "axios";
+import APIendpoint from "../../../constants/constants";
+import { useState } from "react";
+
+export default function Setting_Nickname({userInfo}) {
+  const [nickname, setNickname] = useState("");
+  const [newNickname, setNewNickname] = useState("");
+
+  const handleSetClick = async () => {
+    const access_token = localStorage.getItem("access_token");
+    
+}
+
   return (
     <>
-      <div>nickname</div>
+      <AccountLayout>
+        <Container>
+          <Title>닉네임 설정</Title>
+          <InputBox>
+            <Input placeholder="닉네임" type="text"></Input>
+            <Button>완료</Button>
+          </InputBox>
+        </Container>
+      </AccountLayout>
     </>
   );
 }
+
+const Title = styled.div``;
+
+const Container = styled.div``;
+
+const InputBox = styled.div``;
+
+const Input = styled.input``;
+
+const Button = styled.button``;
--- pages/account/user/index.tsx
+++ pages/account/user/index.tsx
@@ -1,7 +1,11 @@
+import AccountLayout from "../accountLayout";
+
 export default function Setting_User() {
   return (
     <>
-      <div>account</div>
+      <AccountLayout>
+        <div>account</div>
+      </AccountLayout>
     </>
   );
 }
