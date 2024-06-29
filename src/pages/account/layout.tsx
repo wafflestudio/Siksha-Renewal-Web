ce6b3a0400e03acee07916dc7800aa9d67d6b7c3
hwoo-cho04 <henry.chohw04@gmail.com>
2024-06-29 17:42:35 +0900

    fix: match design

--- src/pages/account/layout.tsx
+++ src/pages/account/layout.tsx
@@ -19,7 +19,7 @@ const Content = styled.div`
   width: min(700px, 100%);
   margin: 0 auto;
   margin-top: 37px;
-  height: max(910px, 100vh);
+  height: 100%;
   background: white;
   border-radius: 10px 10px 0 0;
   padding: 20px 19px;
--- src/pages/account/mypost/index.tsx
+++ src/pages/account/mypost/index.tsx
@@ -6,10 +6,9 @@ import { useEffect, useState } from "react";
 import { useStateContext } from "hooks/ContextProvider";
 import { Post, RawPost } from "types";
 import { postParser } from "utils/DataUtil";
+import styled from "styled-components";
 
 export default function MyPost() {
-  const state = useStateContext();
-
   const [posts, setPosts] = useState<Post[]>([]);
 
   function setParsedPosts(rawPost: RawPost) {
@@ -31,9 +30,21 @@ export default function MyPost() {
   useEffect(() => {
     fetchMyPosts();
   }, []);
+
   return (
     <AccountLayout>
+      <Header>내가 쓴 글</Header>
       <PostList posts={posts} />
     </AccountLayout>
   );
 }
+
+const Header = styled.div`
+  margin-top: 23px;
+  margin-bottom: 30px;
+  color: var(--Main-Orange, #ff9522);
+  font-size: 28px;
+  font-weight: 700;
+  line-height: 23px;
+  letter-spacing: -0.3;
+`;
