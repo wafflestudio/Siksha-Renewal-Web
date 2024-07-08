0447c944da66a312a9c3570866ae396aa56237e4
hwoo-cho04 <henry.chohw04@gmail.com>
2024-07-08 22:00:30 +0900

    refactor: single global layout

---
+++ src/components/general/Layout.tsx
@@ -0,0 +1,86 @@
+import axios from "axios";
+import Header from "components/Header";
+import APIendpoint from "constants/constants";
+import { useDispatchContext, useStateContext } from "hooks/ContextProvider";
+import useIsMobile from "hooks/UseIsMobile";
+import { useRouter } from "next/router";
+import React, { useEffect } from "react";
+import styled from "styled-components";
+import { GlobalStyle } from "styles/globalstyle";
+
+interface LayoutProps {
+  children: JSX.Element;
+}
+
+export default function Layout({ children }: LayoutProps) {
+  const router = useRouter();
+  const state = useStateContext();
+  const { setLoginStatus, setUserInfo, setIsFilterFavorite, setIsExceptEmptyRestaurant } =
+    useDispatchContext();
+  const isMobile = useIsMobile();
+
+  const { loginStatus, isExceptEmptyRestaurant } = state;
+
+  useEffect(() => {
+    const access_token = localStorage.getItem("access_token");
+    axios
+      .post(
+        `${APIendpoint()}/auth/refresh`,
+        {},
+        { headers: { "authorization-token": `Bearer ${access_token}` } },
+      )
+      .then((res) => {
+        localStorage.setItem("access_token", res.data.access_token);
+      })
+      .catch((res) => {
+        console.log(res);
+      });
+  }, []);
+
+  useEffect(() => {
+    setLoginStatus(!!localStorage.getItem("access_token"));
+
+    const access_token = localStorage.getItem("access_token");
+
+    async function fetchUserInfo() {
+      try {
+        const res = await axios.get(`${APIendpoint()}/auth/me`, {
+          headers: { "authorization-token": `Bearer ${access_token}` },
+        });
+        setUserInfo({ id: res.data.id, nickname: res.data.nickname });
+      } catch (e) {
+        setUserInfo({ id: null, nickname: null });
+      }
+    }
+
+    fetchUserInfo();
+  }, [loginStatus]);
+
+  useEffect(() => {
+    if (!isMobile) setIsFilterFavorite(false);
+  }, [isMobile]);
+
+  useEffect(() => {
+    if (loginStatus) {
+      const value = localStorage.getItem("isExceptEmptyRestaurant");
+
+      if (value !== null) {
+        setIsExceptEmptyRestaurant(JSON.parse(value));
+      } else {
+        localStorage.setItem("isExceptEmptyRestaurant", JSON.stringify(isExceptEmptyRestaurant));
+      }
+    }
+  }, [loginStatus]);
+
+  return (
+    <>
+      <GlobalStyle />
+      <Container>
+        <Header />
+        {children}
+      </Container>
+    </>
+  );
+}
+
+const Container = styled.div``;
