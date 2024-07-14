c61e88fb9dfcb548ee28f50d7f232c195f078c86
hwoo-cho04 <henry.chohw04@gmail.com>
2024-07-14 10:42:28 +0900

    feat: infinity scroll common layout

---
+++ src/components/general/InfiniteScrollable.tsx
@@ -0,0 +1,53 @@
+import { LegacyRef, ReactNode, useCallback, useEffect, useRef, useState } from "react";
+import styled from "styled-components";
+
+interface InfiniteScrollableProps {
+  fetchMoreData: (size: number, page: number) => Promise<any>;
+  hasNext: boolean;
+  children: JSX.Element[];
+}
+
+export default function InfiniteScrollable({
+  fetchMoreData,
+  hasNext,
+  children,
+}: InfiniteScrollableProps) {
+  const [page, setPage] = useState(1);
+  const [size] = useState(10);
+  const observerElement = useRef<HTMLDivElement | null>(null);
+
+  const observerCallback = useCallback(
+    (entries: IntersectionObserverEntry[]) => {
+      if (entries[0].isIntersecting && hasNext) {
+        setPage((prevPage) => prevPage + 1);
+      }
+    },
+    [hasNext],
+  );
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(observerCallback);
+    if (observerElement.current) {
+      observer.observe(observerElement.current);
+    }
+    return () => {
+      if (observerElement.current) {
+        observer.unobserve(observerElement.current);
+      }
+    };
+  }, [observerCallback]);
+
+  useEffect(() => {
+    fetchMoreData(size, page);
+    console.log(page, "is requested");
+  }, [page]);
+
+  return (
+    <Container>
+      {children}
+      <div style={{ height: "1px" }} ref={observerElement} />
+    </Container>
+  );
+}
+
+const Container = styled.div``;
