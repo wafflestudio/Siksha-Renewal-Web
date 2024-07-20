435a6f1185749a18ac553c21e9642123d575ca5b
B0neh3ad <js1044k@gmail.com>
2024-07-20 17:23:33 +0900

    chore: derive PhotoReviewItem component from phoro review page

--- src/pages/menu/[id]/photos/index.tsx
+++ src/pages/menu/[id]/photos/index.tsx
@@ -1,84 +1,13 @@
+import PhotoReviewItem from "components/MenuDetail/PhotoReviewItem";
 import ReviewItem from "components/MenuDetail/ReviewItem.";
-import Stars from "components/MenuDetail/Stars";
 import MobileSubHeader from "components/MobileSubHeader";
 import { useDispatchContext } from "hooks/ContextProvider";
 import useIsMobile from "hooks/UseIsMobile";
 import { useRouter } from "next/router";
-import { ReviewListType, ReviewType } from "pages/menu/[id]";
+import { ReviewListType } from "pages/menu/[id]";
 import { useEffect, useState } from "react";
-import styled, { ThemeProvider } from "styled-components";
+import styled from "styled-components";
 import { getReviews } from "utils/api/reviews";
-
-function PhotoReviewItem({ review }: { review: ReviewType }) {
-  return (
-    <>
-      <ItemContainer>
-        <Thumbnail src={review.etc.images[0]} />
-        <ReviewInfo>
-          <ReviewerIdText>ID {review.user_id}</ReviewerIdText>
-          <ReviewDate>{review.created_at.substring(0, 10)}</ReviewDate>
-        </ReviewInfo>
-        <ReviewContent>{review.comment}</ReviewContent>
-        <ReviewScore>
-          <ThemeProvider theme={{ width: 73 }}>
-            <Stars score={review.score || 0} />
-          </ThemeProvider>
-          <ReviewScoreValue>{review.score}</ReviewScoreValue>
-        </ReviewScore>
-      </ItemContainer>
-    </>
-  );
-}
-
-const ItemContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  width: 192px;
-  row-gap: 11px;
-`;
-
-const Thumbnail = styled.img`
-  object-fit: cover;
-  height: 192px;
-  width: 192px;
-  border-radius: 8px;
-  width: 192px;
-  height: 192px;
-`;
-
-const ReviewInfo = styled.div`
-  display: flex;
-  justify-content: space-between;
-`;
-
-const ReviewerIdText = styled.span`
-  font-size: 12px;
-  font-weight: 700px;
-`;
-
-const ReviewDate = styled.span`
-  color: #919191;
-  font-size: 10px;
-  font-weight: 700;
-`;
-
-const ReviewContent = styled.p`
-  margin: 0;
-  font-size: 16px;
-  font-weight: 400px;
-`;
-
-const ReviewScore = styled.div`
-  height: 14px;
-  display: flex;
-`;
-
-const ReviewScoreValue = styled.span`
-  color: #ff9522;
-  font-size: 12px;
-  font-weight: 700;
-  margin-left: 10px;
-`;
 
 export default function PhotoReviews() {
   const router = useRouter();
