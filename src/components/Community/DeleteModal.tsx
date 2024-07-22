73c5e0c54df0f54e1d83db9444828f4db9612d54
hwoo-cho04 <henry.chohw04@gmail.com>
2024-07-22 11:05:14 +0900

    feat: delete modal in mobile

--- src/components/Community/DeleteModal.tsx
+++ src/components/Community/DeleteModal.tsx
@@ -1,4 +1,5 @@
 import BackClickable from "components/general/BackClickable";
+import useIsMobile from "hooks/UseIsMobile";
 import styled from "styled-components";
 
 interface DeleteModalProps {
@@ -8,38 +9,59 @@ interface DeleteModalProps {
 }
 
 export default function DeleteModal({ type, onClose, onSubmit }: DeleteModalProps) {
+  const isMobile = useIsMobile();
+
   const target = type === "post" ? "게시물" : "댓글";
 
-  return (
-    <BackClickable onClickBackground={onClose}>
-      <Container>
-        <Header>
-          <Title>{target} 삭제</Title>
-          <CloseButton onClick={onClose}>
-            <Icon src="/img/modal-close.svg" />
-          </CloseButton>
-        </Header>
-        <Message>해당 {target}을 삭제하시겠습니까?</Message>
-        <Footer>
-          <CancelButton onClick={onClose}>취소</CancelButton>
-          <DeleteButton onClick={onSubmit}>삭제</DeleteButton>
-        </Footer>
-      </Container>
-    </BackClickable>
-  );
+  if (!isMobile)
+    return (
+      <BackClickable onClickBackground={onClose}>
+        <DesktopContainer>
+          <Header>
+            <Title>{target} 삭제</Title>
+            <CloseButton onClick={onClose}>
+              <Icon src="/img/modal-close.svg" />
+            </CloseButton>
+          </Header>
+          <Message>해당 {target}을 삭제하시겠습니까?</Message>
+          <Footer>
+            <CancelButton onClick={onClose}>취소</CancelButton>
+            <DeleteButton onClick={onSubmit}>삭제</DeleteButton>
+          </Footer>
+        </DesktopContainer>
+      </BackClickable>
+    );
+  else
+    return (
+      <BackClickable onClickBackground={onClose}>
+        <MobileContainer>
+          <MobileHeader>
+            <MobileTitle>{target} 삭제</MobileTitle>
+          </MobileHeader>
+          <MobileMessage>{target}을 정말 삭제하시겠습니까?</MobileMessage>
+          <MobileFooter>
+            <MobileCancelButton onClick={onClose}>취소</MobileCancelButton>
+            <MobileDeleteButton onClick={onSubmit}>삭제</MobileDeleteButton>
+          </MobileFooter>
+        </MobileContainer>
+      </BackClickable>
+    );
 }
 
 const Container = styled.div`
   display: flex;
   flex-direction: column;
-  gap: 47px 30px;
   position: fixed;
   left: 50%;
-  top: 423px;
   transform: translateX(-50%);
+  box-sizing: border-box;
+`;
+
+const DesktopContainer = styled(Container)`
+  gap: 47px 30px;
+  top: 423px;
   width: 497px;
   height: 230px;
-  box-sizing: border-box;
   border-radius: 13px;
   padding: 27px 30px 30px 33px;
   background-color: white;
@@ -83,4 +105,52 @@ const CancelButton = styled(Button)`
 const DeleteButton = styled(Button)`
   background-color: #ff9522;
   color: white;
+`;
+
+const MobileContainer = styled(Container)`
+  top: 50%;
+  transform: translate(-50%, -50%);
+  gap: 11px 17px;
+  width: calc(100% - 60px);
+  height: 130px;
+  padding-top: 20px;
+  border-radius: 26px;
+  background-color: #fff;
+`;
+const MobileHeader = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+const MobileTitle = styled.div`
+  font-size: 16px;
+  font-weight: 600;
+  line-height: 19px;
+`;
+const MobileMessage = styled.div`
+  display: flex;
+  justify-content: center;
+  font-size: 12px;
+  line-height: 15px;
+`;
+const MobileFooter = styled.div`
+  border-top: 1px solid #e3e3e3;
+  flex: 1;
+`;
+const MobileButton = styled.button`
+  width: 50%;
+  height: 100%;
+  background-color: inherit;
+  border: none;
+  outline: none;
+  font-size: 16px;
+  font-weight: 700;
+  cursor: pointer;
+`;
+const MobileCancelButton = styled(MobileButton)`
+  color: #ff9522;
+  border-right: 1px solid #e3e3e3;
+`;
+const MobileDeleteButton = styled(MobileButton)`
+  color: #797979;
 `;
