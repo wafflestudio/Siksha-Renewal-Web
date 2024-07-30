43beaf2951bcc84f44f8618ccfc14d7e3b7d2acb
hwoo-cho04 <henry.chohw04@gmail.com>
2024-07-30 18:04:54 +0900

    fix: apply infiniteScroll change in comments

--- src/components/Community/CommentList.tsx
+++ src/components/Community/CommentList.tsx
@@ -2,18 +2,18 @@ import styled from "styled-components";
 import { Comment as CommentType } from "types";
 import Comment from "components/Community/Comment";
 import InfiniteScrollable from "components/general/InfiniteScrollable";
+import { useState } from "react";
 
 interface CommentsProps {
   comments: CommentType[];
-  fetch: (size: number, page: number) => Promise<void>;
+  fetch: (size: number, page: number) => Promise<boolean | void>;
   update: (id: number) => void;
-  hasNext: boolean;
 }
 
-export default function CommentList({ comments, fetch, update, hasNext }: CommentsProps) {
+export default function CommentList({ comments, fetch, update }: CommentsProps) {
   return (
     <Container>
-      <InfiniteScrollable fetchMoreData={fetch} hasNext={hasNext}>
+      <InfiniteScrollable fetchMoreData={fetch}>
         {comments.map((comment, i) => (
           <Comment key={i} comment={comment} update={update} />
         ))}
