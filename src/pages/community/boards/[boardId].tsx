1094b5a416c155b9513ae74ab008ac7b6df6646c
hwoo-cho04 <henry.chohw04@gmail.com>
2024-07-30 18:05:22 +0900

    fix: delete deprecated hook

--- src/pages/community/boards/[boardId].tsx
+++ src/pages/community/boards/[boardId].tsx
@@ -8,15 +8,15 @@ import Board from ".";
 import { useRouter } from "next/router";
 import { postParser } from "utils/DataUtil";
 import { getPostList } from "utils/api/community";
-import UseAccessToken from "hooks/UseAccessToken";
 import MobileNavigationBar from "components/general/MobileNavigationBar";
+import useAuth from "hooks/UseAuth";
 
 export default function Posts() {
   const router = useRouter();
   const { boardId } = router.query;
   const [posts, setPosts] = useState<Post[]>([]);
 
-  const { checkAccessToken } = UseAccessToken();
+  const { checkAccessToken } = useAuth();
 
   const fetchPosts = (size: number, page: number) => {
     return checkAccessToken()
