49d97859a04f6d0ada4f5f24f3b9adc81501260b
hwoo-cho04 <henry.chohw04@gmail.com>
2024-07-31 13:15:07 +0900

    fix: 모달 onClose onSubmit 파라미터 전달 가능하게

--- src/components/general/Modals.tsx
+++ src/components/general/Modals.tsx
@@ -6,27 +6,34 @@ export default function Modals() {
   const openedModals = useContext(ModalsStateContext);
   const { closeModal } = useModals();
 
-  return openedModals.map((modal, i) => {
-    const { Component, props } = modal;
+  return (
+    <>
+      {openedModals.map((modal, i) => {
+        const { Component, props } = modal;
 
-    const { onClose, onSubmit, ...rest } = props;
+        const { onClose, onSubmit, ...rest } = props;
 
-    const handleClose = () => {
-      onClose();
-      closeModal(Component);
-    };
+        const handleClose = (...closeArgs: any[]) => {
+          if (typeof onClose === "function") {
+            onClose(...closeArgs);
+          }
+          closeModal(Component);
+        };
 
-    const handleSubmit = async () => {
-      if (typeof onSubmit === "function") {
-        await onSubmit();
-      }
-      handleClose();
-    };
+        const handleSubmit = async (...submitArgs: any[]) => {
+          if (typeof onSubmit === "function") {
+            await onSubmit(...submitArgs);
+          }
+          // handleClose의 parameter들을 어떻게 전달해줄지 고민입니다.
+          handleClose();
+        };
 
-    return (
-      i === openedModals.length - 1 && (
-        <Component key={i} {...rest} onClose={handleClose} onSubmit={handleSubmit} />
-      )
-    );
-  });
+        return (
+          i === openedModals.length - 1 && (
+            <Component key={i} {...rest} onClose={handleClose} onSubmit={handleSubmit} />
+          )
+        );
+      })}
+    </>
+  );
 }
