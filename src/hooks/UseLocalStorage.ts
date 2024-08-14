6f58051a4cfc3e3286a77b151a0b670c162486ea
hwoo-cho <henry.chohw@gmail.com>
2024-08-14 23:37:58 +0900

    feat: localStorage manager hook

---
+++ src/hooks/UseLocalStorage.ts
@@ -0,0 +1,38 @@
+import { useCallback, useSyncExternalStore } from "react";
+
+export default function useLocalStorage(key: string, initialValue: any) {
+  // set localStorage item
+  const setStorage = useCallback(
+    (newValue: any) => {
+      localStorage.setItem(key, JSON.stringify(newValue));
+      dispatchEvent(new StorageEvent("storage", { key: key, newValue }));
+    },
+    [key],
+  );
+
+  // remove localStorage item
+  const removeStorage = useCallback(() => {
+    localStorage.removeItem(key);
+    dispatchEvent(new StorageEvent("storage", { key: key }));
+  }, [key]);
+
+  // get localStorage item
+  const getSnapshot = () => localStorage.getItem(key);
+
+  //get temporary item value for server side rendering
+  const getServerSnapshot = () => initialValue;
+
+  // subscribe localStorage item to react change
+  const subsribe = (listener: () => void) => {
+    window.addEventListener("storage", listener);
+    return () => window.removeEventListener("storage", listener);
+  };
+
+  const store = useSyncExternalStore(subsribe, getSnapshot, getServerSnapshot);
+
+  return {
+    value: store,
+    set: setStorage,
+    remove: removeStorage,
+  };
+}
