881a7ab9c9a140e34972ea5ad7b1eb3c5bd97234
hwoo-cho <henry.chohw@gmail.com>
2024-08-15 13:34:33 +0900

    fix: simpler  initialize logic

--- src/hooks/UseFavorite.ts
+++ src/hooks/UseFavorite.ts
@@ -6,23 +6,12 @@ import useAuth from "./UseAuth";
 export default function useFavorite() {
   const { authStatus } = useAuth();
 
-  const [favoriteRestaurants, setFavoriteRestaurants] = useState<number[]>([]);
-
   const { openLoginModal } = useModals();
 
-  // favoriteList는 로컬 스토리지에 저장된 favorite restaurant id list입니다
   const { value, set: setStorage } = useLocalStorage("favorite_restaurant", "[]");
-  const favoriteList = JSON.parse(value);
+  const parsedValue = JSON.parse(value);
 
-  useEffect(() => {
-    if (authStatus === "logout") {
-      setFavoriteRestaurants([]);
-      return;
-    }
-
-    // 초깃값 세팅
-    setFavoriteRestaurants(favoriteList);
-  }, [authStatus]);
+  const [favoriteRestaurants, setFavoriteRestaurants] = useState<number[]>(parsedValue);
 
   const toggleFavorite = (restaurantId: number) => {
     if (authStatus === "logout") openLoginModal();
--- src/hooks/UseIsAnonymousWriter.ts
+++ src/hooks/UseIsAnonymousWriter.ts
@@ -1,21 +1,11 @@
-import { useEffect, useState } from "react";
-import { useDispatchContext, useStateContext } from "./ContextProvider";
+import { useState } from "react";
 import useLocalStorage from "./UseLocalStorage";
-import useAuth from "./UseAuth";
 
 export default function useIsAnonymousWriter() {
-  const { authStatus } = useAuth();
-
-  const [isAnonymousWriter, setIsAnonymousWriter] = useState<boolean>(false);
-
   const { value, set: setStorage } = useLocalStorage("isAnonymousWriter", "false");
-  const initailValue = JSON.parse(value);
+  const parsedValue = JSON.parse(value);
 
-  useEffect(() => {
-    if (authStatus === "login") {
-      setIsAnonymousWriter(initailValue);
-    }
-  }, [authStatus]);
+  const [isAnonymousWriter, setIsAnonymousWriter] = useState<boolean>(parsedValue);
 
   function toggleIsAnonymousWriter() {
     setIsAnonymousWriter(!isAnonymousWriter);
