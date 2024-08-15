32ccf4220e53b369cdefcb8cd61d39a97a769651
hwoo-cho <henry.chohw@gmail.com>
2024-08-15 13:32:19 +0900

    fix: restaurants 불러오기 실패 오류 수정

--- src/utils/api/restaurants.ts
+++ src/utils/api/restaurants.ts
@@ -1,6 +1,7 @@
 import axios from "axios";
 import APIendpoint from "constants/constants";
 import { Restaurant, RawRestaurant } from "types";
+import { restaurantParser } from "utils/DataUtil";
 
 export const getRestaurantList = (): Promise<{
   count: number;
@@ -9,20 +10,14 @@ export const getRestaurantList = (): Promise<{
   return axios
     .get(`${APIendpoint()}/restaurants/`)
     .then((res) => {
-      const { data: rawData } = res;
-      const data = rawData.map((restaurant: RawRestaurant) => ({
-        createdAt: restaurant.created_at,
-        updatedAt: restaurant.updated_at,
-        id: restaurant.id,
-        code: restaurant.code,
-        nameKr: restaurant.name_kr,
-        nameEn: restaurant.name_en,
-        addr: restaurant.addr,
-        lat: restaurant.lat,
-        lng: restaurant.lng,
-        etc: restaurant.etc,
-      }));
-      return data;
+      const { count, result: rawData } = res.data;
+
+      const data = rawData.map(restaurantParser);
+
+      return {
+        count: count,
+        result: data,
+      };
     })
     .catch((e) => {
       throw new Error(e);
