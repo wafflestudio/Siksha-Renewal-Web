1a324fa92375db781dfedd44da89d713b80d06fa
hwoo-cho <henry.chohw@gmail.com>
2024-08-24 23:32:51 +0900

    fix: cleaner code

--- src/hooks/UseFavorite.ts
+++ src/hooks/UseFavorite.ts
@@ -11,7 +11,7 @@ export default function useFavorite() {
 
   const { value, set: setStorage } = useLocalStorage("favorite_restaurant", "[]");
   // localStorage가 구독되어 변화를 감지하므로, 따로 state를 만들어주기 보다는 JSON parse 결과를 바로 이용해야 합니다.
-  const favoriteRestaurants: number[] = JSON.parse(value ? value : "[]");
+  const favoriteRestaurants: number[] = JSON.parse(value || "[]");
 
   const toggleFavorite = (restaurantId: number) => {
     if (authStatus === "logout") openLoginModal();
--- src/hooks/UseOrder.ts
+++ src/hooks/UseOrder.ts
@@ -7,7 +7,7 @@ export default function useOrder(type: "favorite" | "nonFavorite") {
   const key = type === "favorite" ? "orderList_favorite" : "orderList_nonFavorite";
 
   const { value, set: setStorage } = useLocalStorage(key, "[]");
-  const orderList = JSON.parse(value ? value : "[]");
+  const orderList = JSON.parse(value || "[]");
 
   function setNewOrderList(newOrderList: RestaurantPreview[]) {
     setStorage(JSON.stringify(newOrderList));
