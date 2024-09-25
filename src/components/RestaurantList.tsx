aac6ed0bbadf3ad599b428ad79d587bdcd9dded2
B0neh3ad <js1044k@gmail.com>
2024-09-25 16:07:44 +0900

    chore: remove 'For' from sanitizer function name

--- src/components/RestaurantList.tsx
+++ src/components/RestaurantList.tsx
@@ -2,10 +2,10 @@ import styled from "styled-components";
 import { useStateContext } from "context/ContextProvider";
 import { useEffect, useState } from "react";
 import useFavorite from "hooks/UseFavorite";
-import { sanitizeForCssSelector } from "utils/FormatUtil";
+import { sanitizeCssSelector } from "utils/FormatUtil";
 
 function scrollRestaurant(restaurant) {
-  let element = document.querySelector(".a" + sanitizeForCssSelector(restaurant));
+  let element = document.querySelector(".a" + sanitizeCssSelector(restaurant));
   if (!element) {
     throw new Error("Cannot find element");
   }
