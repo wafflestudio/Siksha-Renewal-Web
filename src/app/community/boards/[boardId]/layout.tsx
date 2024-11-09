f8cfb464ccc59717b2f38be1314c49c15080c550
MinnieMinwoo <nhs075241@gmail.com>
2024-11-09 23:51:45 +0900

    fix: fix board visible state

---
+++ src/app/community/boards/[boardId]/layout.tsx
@@ -0,0 +1,14 @@
+import { getBoardList } from "utils/api/community";
+import { BoardMenu } from "./components/BoardMenu";
+import { boardParser } from "utils/DataUtil";
+
+export default async function CommunityLayout({ params, children }) {
+  const { boardId } = (await params) ?? "";
+  const boards = await getBoardList().then((rawBoardList) => rawBoardList.map(boardParser));
+
+  return (
+    <>
+      <BoardMenu boardId={Number(boardId)} boards={boards} />;{children}
+    </>
+  );
+}
