ceafb96d0b6fe7c2f8a1e44519809f530e7f9846
B0neh3ad <js1044k@gmail.com>
2024-11-10 10:57:23 +0900

    feat: add generateStaticParams()

--- src/app/menu/[menuId]/page.tsx
+++ src/app/menu/[menuId]/page.tsx
@@ -1,237 +1,34 @@
-'use client'
+import { getMenuList } from "utils/api/menus";
+import Menu from "./Menu";
 
-import styled from "styled-components";
-import React, { useEffect, useState } from "react";
-import { useRouter, useParams } from 'next/navigation';
-import ReviewPostModal from "components/MenuDetail/ReviewPostModal";
-import MobileSubHeader from "components/general/MobileSubHeader";
-import MobileNavigationBar_Legacy from "components/general/MobileNavigationBar_Legacy";
-import { getMenu } from "utils/api/menus";
-import { getReviews } from "utils/api/reviews";
-import MenuSection from "components/MenuDetail/MenuSection";
-import ReviewSection from "components/MenuDetail/ReviewSection";
-import useModals from "hooks/UseModals";
-import useAuth_Legacy from "hooks/UseAuth_Legacy";
-
-export interface MenuType {
-  id: number;
-  restaurant_id: number;
-  code: string;
-  date: string;
-  type: string;
-  name_kr: string;
-  name_en: string;
-  price: number;
-  etc: {};
-  created_at: string;
-  updated_at: string;
-  score: number | null;
-  review_cnt: number;
-  is_liked: boolean;
-  like_cnt: number;
-}
-
-export interface ReviewType {
-  id: number;
-  menu_id: number;
-  user_id: number;
-  score: number | null;
-  comment: string;
-  etc: Record<string, any>;
-  created_at: string;
-  updated_at: string;
-}
-
-export interface ReviewListType {
-  result: ReviewType[];
-  total_count: number;
-}
-
-export default function Menu() {
-  const router = useRouter();
-  const params = useParams();
-  const menuId = params?.menuId;
-  const [isLoading, setLoading] = useState(false);
-  const [reviews, setReviews] = useState<ReviewListType>({
-    result: [],
-    total_count: 0,
-  });
-  const [menu, setMenu] = useState<MenuType>();
-  const [images, setImages] = useState<string[]>([]);
-  const [isReviewPostModalOpen, setIsReviewPostModalOpen] = useState(false);
-
-  const { openLoginModal } = useModals();
-
-  const [mobileSubHeaderTitle, setMobileSubHeaderTitle] = useState<string>("");
-  const [isReviewListPageOpen, setIsReviewListPageOpen] = useState<boolean>(false);
-
-  const { authStatus, getAccessToken } = useAuth_Legacy();
-
-  const fetchMenu = async () => {
-    const accessToken = await getAccessToken().catch((error) => "");
-
-    getMenu(Number(menuId), accessToken)
-      .then((menuData) => {
-        setMenu(menuData);
-        setMobileSubHeaderTitle(menuData.name_kr);
-      })
-      .catch((e) => {
-        console.error(e);
-        router.push("/");
-      });
-  };
-
-  const fetchReviews = async () => {
-    getReviews(Number(menuId))
-      .then((reviewsData) => {
-        setReviews({
-          result: reviewsData.result,
-          total_count: reviewsData.totalCount,
+/**
+ * 모든 메뉴의 id를 가져와야 하나 사실상 불가능하므로, 일단 build된 날짜의
+ * 메뉴 id만 staticParams에 포함시켰습니다. 추후 EC2 + docker로 수정 시
+ * 리뷰 페이지 정상 접속을 위해 이 부분은 반드시 수정되어야 합니다!
+ */
+export async function generateStaticParams() {
+  const dateString = new Date().toISOString().split("T")[0];
+  const staticParams: { menuId: string }[] = [];
+  const { result } = await getMenuList(dateString, true);
+  result.map((rawMenuList) => {
+    ["BR", "LU", "DN"].forEach((key) => {
+      rawMenuList[key].map(({ menus }) => {
+        menus.map(({ id: menuId }) => {
+          staticParams.push({ menuId: menuId.toString() })
         });
-      })
-      .catch((e) => {
-        console.error(e);
-        router.push("/");
       });
-  };
-
-  useEffect(() => {
-    if (!menuId) {
-      return;
-    }
-    setLoading(true);
-
-    async function fetchData() {
-      Promise.all([fetchMenu(), fetchReviews()])
-        .then(() => {})
-        .catch((e) => {})
-        .finally(() => setLoading(false));
-    }
-    fetchData();
-  }, [menuId, setLoading]);
-
-  useEffect(() => {
-    var updatedImages: string[] = [];
-    reviews.result.map((review) => {
-      if (review.etc) {
-        updatedImages = updatedImages.concat(review.etc.images);
-      }
     });
-    setImages(updatedImages);
-  }, [reviews]);
+  });
 
-  const handleReviewPostButtonClick = () => {
-    if (authStatus === "login") handleReviewPostModal(true);
-    else openLoginModal();
-  };
+  return staticParams;
+}
 
-  const handleReviewPostModal = (isOpen: boolean) => {
-    if (!menu) {
-      console.error("menu is not loaded");
-      return;
-    }
-    setMobileSubHeaderTitle(isOpen ? "나의 평가 남기기" : menu.name_kr);
-    setIsReviewPostModalOpen(isOpen);
-  };
-
-  const handleReviewListPage = (isOpen: boolean) => {
-    if (!menu) {
-      console.error("menu is not loaded");
-      return;
-    }
-    setMobileSubHeaderTitle(isOpen ? "리뷰" : menu.name_kr);
-    setIsReviewListPageOpen(isOpen);
-  };
-
-  const handleMobileSubHeaderBack = () => {
-    if (isReviewPostModalOpen) {
-      handleReviewPostModal(false);
-    } else if (isReviewListPageOpen) {
-      handleReviewListPage(false);
-    } else {
-      router.push("/");
-    }
-  };
+export default async function Page({ params }) {
+  const { menuId } = (await params) ?? "";
 
   return (
     <>
-      {!isLoading && !!menu && (
-        <>
-          <MobileSubHeader title={mobileSubHeaderTitle} handleBack={handleMobileSubHeaderBack} />
-          <Background>
-            <Info>
-              <MenuSection
-                menu={menu}
-                reviewsTotalCount={reviews.total_count}
-                images={images}
-                handleReviewPostButtonClick={handleReviewPostButtonClick}
-                isReviewListPageOpen={isReviewListPageOpen}
-              />
-              <MobileHLine $isReviewListPageOpen={isReviewListPageOpen} />
-              {isReviewPostModalOpen ? (
-                <ReviewPostModal
-                  isOpen={isReviewPostModalOpen}
-                  menu={{
-                    menuName: menu.name_kr,
-                    menuId: menu.id,
-                  }}
-                  onSubmit={fetchReviews}
-                  onClose={() => handleReviewPostModal(false)}
-                />
-              ) : (
-                <ReviewSection
-                  menu={menu}
-                  reviews={reviews}
-                  images={images}
-                  isReviewListPageOpen={isReviewListPageOpen}
-                  handleReviewPostButtonClick={handleReviewPostButtonClick}
-                  handleReviewListPage={handleReviewListPage}
-                />
-              )}
-            </Info>
-          </Background>
-          <MobileNavigationBar_Legacy />
-        </>
-      )}
+      <Menu menuId={Number(menuId)} />
     </>
   );
 }
-
-const Background = styled.div`
-  background-color: white;
-  overflow: scroll;
-  display: flex;
-  flex-direction: column;
-
-  @media (max-width: 768px) {
-    height: 100%;
-    flex-direction: column;
-  }
-`;
-
-const Info = styled.div`
-  position: relative;
-  background-color: white;
-  display: flex;
-  justify-content: center;
-  height: max(809px, calc(100vh - 271px));
-  width: 100%;
-  margin: 0 auto;
-  @media (max-width: 768px) {
-    flex-direction: column;
-    height: max(724px, calc(100vh - 60px));
-    width: 100%;
-    margin: 0;
-  }
-`;
-
-const MobileHLine = styled.div<{ $isReviewListPageOpen: boolean }>`
-  display: none;
-  background: #9191911a;
-  padding: 5px 0;
-  margin-bottom: 16px;
-  @media (max-width: 768px) {
-    display: inherit;
-  }
-  ${(props) => props.$isReviewListPageOpen && `display:none;`}
-`;
--- src/app/menu/[menuId]/photos/page.tsx
+++ src/app/menu/[menuId]/photos/page.tsx
@@ -1,202 +1,34 @@
-'use client'
+import { getMenuList } from "utils/api/menus";
+import PhotoReviews from "./PhotoReviews";
 
-import PhotoReviewItem from "components/MenuDetail/PhotoReviewItem";
-import ReviewItem from "components/MenuDetail/ReviewItem.";
-import MobileSubHeader from "components/general/MobileSubHeader";
-import useAuth from "hooks/UseAuth";
-import useIsMobile from "hooks/UseIsMobile";
-import useModals from "hooks/UseModals";
-import { useRouter, useParams } from 'next/navigation';
-import { ReviewListType } from "app/menu/[menuId]/page";
-import { useEffect, useState } from "react";
-import styled from "styled-components";
-import { getReviews } from "utils/api/reviews";
-
-export default function PhotoReviews() {
-  const router = useRouter();
-  const params = useParams();
-  const menuId = params?.menuId;
-  const [reviews, setReviews] = useState<ReviewListType>({
-    result: [],
-    total_count: 0,
+/**
+ * 모든 메뉴의 id를 가져와야 하나 사실상 불가능하므로, 일단 build된 날짜의
+ * 메뉴 id만 staticParams에 포함시켰습니다. 추후 EC2 + docker로 수정 시
+ * 리뷰 페이지 정상 접속을 위해 이 부분은 반드시 수정되어야 합니다!
+ */
+export async function generateStaticParams() {
+  const dateString = new Date().toISOString().split("T")[0];
+  const staticParams: { menuId: string }[] = [];
+  const { result } = await getMenuList(dateString, true);
+  result.map((rawMenuList) => {
+    ["BR", "LU", "DN"].forEach((key) => {
+      rawMenuList[key].map(({ menus }) => {
+        menus.map(({ id: menuId }) => {
+          staticParams.push({ menuId: menuId.toString() })
+        });
+      });
+    });
   });
 
-  const { openLoginModal } = useModals();
-  const isMobile = useIsMobile();
-  const mobileSubHeaderTitle = "사진 리뷰 모아보기";
-  const { getAccessToken } = useAuth();
+  return staticParams;
+}
 
-  useEffect(() => {
-    if (!menuId) {
-      return;
-    }
-
-    const fetchReview = () => {
-      getReviews(Number(menuId))
-        .then(({ totalCount, result }) => {
-          const photoReviews = result.filter((review) => review.etc);
-          setReviews({
-            result: photoReviews,
-            total_count: photoReviews.length,
-          });
-        })
-        .catch((e) => {
-          console.error(e);
-          router.push("/");
-        });
-    };
-
-    fetchReview();
-  }, [menuId]);
-
-  const handleReviewPostButtonClick = () => {
-    getAccessToken()
-      .then(() => router.push(`/menu/${menuId}?writeReview=true`))
-      .catch(() => openLoginModal());
-  };
-
-  const handleMobileSubHeaderBack = () => router.push(`/menu/${menuId}`);
+export default async function Page({ params }) {
+  const { menuId } = (await params) ?? "";
 
   return (
     <>
-      <MobileSubHeader title={mobileSubHeaderTitle} handleBack={handleMobileSubHeaderBack} />
-      <Container>
-        <GalleryWrapper>
-          <GalleryHeader>
-            <GalleryTitle>사진 리뷰</GalleryTitle>
-            <PhotoReviewsCount>{reviews.total_count}</PhotoReviewsCount>
-          </GalleryHeader>
-          {reviews.result.length > 0 ? (
-            <Gallery>
-              {reviews.result.map((review) =>
-                isMobile ? (
-                  <ReviewItem key={review.id} review={review} />
-                ) : (
-                  <PhotoReviewItem key={review.id} review={review} />
-                ),
-              )}
-            </Gallery>
-          ) : (
-            <NoReviewMessage>아직 등록된 리뷰가 없어요.</NoReviewMessage>
-          )}
-          <ReviewPostButtonWrapper>
-            <ReviewPostButton onClick={handleReviewPostButtonClick} mobile={true}>
-              나의 평가 남기기
-            </ReviewPostButton>
-          </ReviewPostButtonWrapper>
-        </GalleryWrapper>
-      </Container>
+    <PhotoReviews menuId={menuId} />
     </>
   );
 }
-
-const Container = styled.div`
-  position: relative;
-  background-color: white;
-  width: 100%;
-  min-height: calc(100vh - 271px);
-  @media (max-width: 768px) {
-    height: 100%;
-    overflow-y: scroll;
-  }
-`;
-
-const GalleryWrapper = styled.main`
-  position: relative;
-  display: flex;
-  flex-direction: column;
-  width: 862px;
-  min-height: calc(100vh - 271px);
-  padding: 32px 0;
-  margin: auto;
-  box-sizing: border-box;
-  @media (max-width: 768px) {
-    width: 100%;
-    padding: 0 16px;
-    height: 100%;
-  }
-`;
-
-const GalleryHeader = styled.div`
-  display: flex;
-  @media (max-width: 768px) {
-    display: none;
-  }
-`;
-
-const GalleryTitle = styled.h1`
-  font-size: 24px;
-  font-weight: 700;
-  margin: 0;
-`;
-
-const PhotoReviewsCount = styled.div`
-  margin-left: 4px;
-  margin-top: 2px;
-  color: #ff9522;
-  font-size: 16px;
-  font-weight: 800;
-`;
-
-const Gallery = styled.section`
-  display: grid;
-  width: 100%;
-  padding-top: 31px;
-  grid-template-columns: repeat(auto-fill, 192px);
-  column-gap: 30px;
-  row-gap: 38px;
-  @media (max-width: 768px) {
-    padding-top: 24px;
-    display: flex;
-    flex-direction: column;
-    width: 100%;
-    overflow-y: scroll;
-    overflow-x: auto;
-  }
-`;
-
-const NoReviewMessage = styled.div`
-  display: flex;
-  flex-direction: column;
-  text-align: center;
-  width: 100%;
-  font-size: 20px;
-  font-weight: 400;
-  color: #797979;
-  flex-grow: 1;
-  justify-content: center;
-  @media (max-width: 768px) {
-    font-size: 18px;
-  }
-`;
-
-const ReviewPostButtonWrapper = styled.div`
-  position: sticky;
-  top: 0;
-  width: 100%;
-  height: 100%;
-  height: 46px;
-  bottom: 17px;
-  @media (max-width: 768px) {
-    display: none;
-  }
-`;
-
-const ReviewPostButton = styled.button<{ mobile: boolean }>`
-  position: absolute;
-  left: 50%;
-  transform: translateX(-50%);
-  background: #ff9522;
-  width: 200px;
-
-  padding: 14px 25px;
-  border: none;
-  border-radius: 5px;
-
-  color: white;
-  text-align: center;
-  font-size: 16px;
-  font-weight: 700;
-  line-height: 18px;
-  cursor: pointer;
-`;
