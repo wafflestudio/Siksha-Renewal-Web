ef16de4508b24b5b43c2bd11ed287efcfdff53b0
B0neh3ad <js1044k@gmail.com>
2024-12-29 17:02:25 +0900

    feat: Add generateStaticParams

--- src/app/menu/[menuId]/page.tsx
+++ src/app/menu/[menuId]/page.tsx
@@ -1,4 +1,22 @@
+import { getMenuList } from "utils/api/menus";
 import Menu from "./Menu";
+
+export async function generateStaticParams() {
+  const dateString = new Date().toISOString().split("T")[0];
+  const staticParams: { menuId: string }[] = [];
+  const { result } = await getMenuList(dateString, true);
+  result.map((rawMenuList) => {
+    ["BR", "LU", "DN"].forEach((key) => {
+      rawMenuList[key].map(({ menus }) => {
+        menus.map(({ id: menuId }) => {
+          staticParams.push({ menuId: menuId.toString() })
+        });
+      });
+    });
+  });
+
+  return staticParams;
+}
 
 export default async function Page({ params }) {
   const { menuId } = (await params) ?? "";
