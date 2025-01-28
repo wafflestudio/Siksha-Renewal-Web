dafa428ccca605d336d2e4c96b9cf1e66be0697e
B0neh3ad <js1044k@gmail.com>
2025-01-28 17:13:44 +0900

    Feat: Add TwoColumnLayout

---
+++ src/styles/layouts/TwoColumnLayout.tsx
@@ -0,0 +1,43 @@
+import { ReactNode } from "react";
+import styled from "styled-components";
+import { layoutStyle } from "./layout.types";
+
+type LayoutProps = { children: ReactNode } & layoutStyle;
+
+const TwoColumnLayout = styled.div<LayoutProps>`
+  box-sizing: border-box;
+  display: flex;
+  margin: 0 auto;
+  padding: 0 24px;
+  max-width: 1242px;
+  gap: 30px;
+  height: ${(props) => props.height || "auto"};
+
+  @media (max-width: 900px) {
+    padding: 0 16px;
+  }
+
+  @media (max-width: 768px) {
+    display: none; /* 데스크톱 전용 layout */
+  }
+`;
+
+const Left = styled.div`
+  flex: 0 0 378px;
+
+  @media (max-width: 900px) {
+    flex: 0 0 360px;
+  }
+`;
+
+const Right = styled.div`
+  flex: 1;
+  max-width: 786px;
+  min-width: 0;
+
+  @media(max-width: 900px) {
+    max-width: 445px;
+  }
+`;
+
+export { TwoColumnLayout, Left, Right };
---
+++ src/styles/layouts/layout.types.tsx
@@ -0,0 +1,6 @@
+export type layoutStyle = {
+    margin?: number | string;
+    padding?: number | string;
+    width?: number | string;
+    height?: number | string;
+}
