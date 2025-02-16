f21e795d026b2bb1e409c92f598aae39ef240fcd
테스트 <teseuteu@teseuteuui-MacBookPro.local>
2025-02-17 04:44:06 +0900

    feat: add bottom-sheet ui

---
+++ src/app/components/MobileFilter/MobileBottomSheet.tsx
@@ -0,0 +1,60 @@
+import { useState } from "react";
+import styled from "styled-components";
+
+interface MobileBottomSheetProps {
+  children: React.ReactNode;
+  isOpen: boolean;
+  onClose: () => void;
+}
+
+export default function MobileBottomSheet({ children, isOpen, onClose }: MobileBottomSheetProps) {
+  if (!isOpen) return null;
+
+  return (
+    <>
+      <BottomSheetBackdrop onClick={onClose} />
+      <BottomSheetWrapper>
+        <BottomSheetSlideBar />
+        <BottomSheetContent>{children}</BottomSheetContent>
+      </BottomSheetWrapper>
+    </>
+  );
+}
+
+const BottomSheetBackdrop = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  bottom: 0;
+  right: 0;
+  background-color: rgba(0, 0, 0, 0.5);
+  z-index: 99;
+`;
+
+const BottomSheetSlideBar = styled.div`
+  width: 42px;
+  height: 4px;
+  background-color: #dfdfdf;
+  border-radius: 2px;
+  margin: 10px auto;
+`;
+
+const BottomSheetContent = styled.div`
+  padding: 20px;
+`;
+
+const BottomSheetWrapper = styled.div`
+  position: fixed;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  background-color: white;
+  max-height: 80vh;
+  overflow-y: auto;
+  border-top-left-radius: 20px;
+  border-top-right-radius: 20px;
+  box-shadow: 0px -4px 8px rgba(0, 0, 0, 0.1);
+  z-index: 100;
+  padding-top: 17px;
+  transition: transform 0.3s;
+`;
