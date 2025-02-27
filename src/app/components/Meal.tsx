c664763ba64b5f326c283638514c03fcccb3d172
B0neh3ad <js1044k@gmail.com>
2025-02-27 23:53:32 +0900

    Fix: fix ui of Meal component distorted by merging with dev-master

--- src/app/components/Meal.tsx
+++ src/app/components/Meal.tsx
@@ -61,6 +61,8 @@ const Container = styled.div`
   }
 
   @media (max-width: 768px) {
+    justify-content: center;
+    align-items: flex-end;
     padding-top: 15.28px;
     padding-bottom: 17px;
   }
