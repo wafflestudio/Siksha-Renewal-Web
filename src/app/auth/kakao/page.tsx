7ca879ccaadbd5282789eadb24b3ef23979d2b96
B0neh3ad <js1044k@gmail.com>
2025-03-27 13:18:20 +0900

    chore: redirect previous page after succeeding login

--- src/app/auth/kakao/page.tsx
+++ src/app/auth/kakao/page.tsx
@@ -22,7 +22,7 @@ export default function Auth() {
     loginKakao(code)
       .then((accessToken) => {
         login(accessToken);
-        router.push("/");
+        router.back();
       })
       .catch(onHttpError);
   }, []);
