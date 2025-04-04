96e32002157fe9ed8dc6f1147b5cd63858b3e067
B0neh3ad <js1044k@gmail.com>
2025-04-04 16:25:32 +0900

    fix: update menucard header design

--- src/app/components/MenuCard.tsx
+++ src/app/components/MenuCard.tsx
@@ -1,6 +1,3 @@
-`use client`;
-import { useRef, useState } from "react";
-
 import styled from "styled-components";
 import Menu from "app/components/Menu";
 import { useDispatchContext, useStateContext } from "providers/ContextProvider";
@@ -10,7 +7,6 @@ import { sanitizeCssSelector } from "utils/FormatUtil";
 import OperatingHour from "./OperatingHour";
 import { RawMenu, RawRestaurant } from "types";
 import getCurrentOperatingHours from "utils/getCurrentOperatingHours";
-import { useEffect } from "react";
 
 type Data = RawRestaurant & {
   menus: RawMenu[];
@@ -107,6 +103,17 @@ export default function MenuCard({ data }: { data: Data }) {
             </TitleIconList>
           </TitleContainer>
           <InfoContainer>
+            <OperatingHour
+              type={meal}
+              hour={getCurrentOperatingHours(
+                meal,
+                data.etc?.operating_hours ?? {
+                  weekdays: [],
+                  saturday: [],
+                  holiday: [],
+                },
+              )} //data.etc가 null인 case가 있음
+            />
             <HeaderDataList>
               <HeaderDataText>Price</HeaderDataText>
               <HeaderDataText>Rate</HeaderDataText>
@@ -177,9 +184,7 @@ const HeaderContainer = styled.div`
   gap: 8px;
 
   @media (max-width: 768px) {
-    flex-wrap: inherit;
     gap: 11px;
-    justify-content: space-between;
   }
 `;
 
@@ -209,9 +214,7 @@ const InfoContainer = styled.div`
   }
 
   @media (max-width: 768px) {
-    min-width: 0;
-    flex: 0 0 auto;
-    align-items: flex-start;
+    justify-content: space-between;
   }
 `;
 
@@ -250,6 +253,13 @@ const HeaderDataText = styled.p<{ disableWidth?: number; shrinkWidth?: number }>
 
   @media (max-width: 768px) {
     width: fit-content;
+
+    /* text-12/Regular */
+    font-family: var(--Font-family-sans, NanumSquareOTF);
+    font-size: var(--Font-size-12, 12px);
+    font-style: normal;
+    font-weight: var(--Font-weight-regular, 400);
+    line-height: 140%; /* 16.8px */
   }
 `;
 
@@ -277,6 +287,18 @@ const Name = styled.div`
   font-style: normal;
   font-weight: var(--Font-weight-extrabold, 800);
   line-height: 140%; /* 25.2px */
+
+  @media (max-width: 768px) {
+    color: var(--Color-Foundation-base-black, #000);
+
+    /* text-16/ExtraBold */
+    font-family: var(--Font-family-sans, NanumSquareOTF);
+    font-size: var(--Font-size-16, 16px);
+    font-style: normal;
+    font-weight: var(--Font-weight-extrabold, 800);
+    line-height: 140%; /* 22.4px */
+    letter-spacing: var(--Font-letter-spacing-0, -0.3px);
+  }
 `;
 
 const TitleIconList = styled.div`
