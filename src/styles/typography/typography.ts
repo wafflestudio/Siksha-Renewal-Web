dcb9736c33bbb398bb66130c615111d34b427a42
IceCandle <icecandle2413@gmail.com>
2025-05-04 15:17:27 +0900

    Suggest both options of text component or typography.ts

---
+++ src/styles/typography/typography.ts
@@ -0,0 +1,34 @@
+import styled from 'styled-components';
+
+export const Text = {
+    Text11Regular: styled.span`
+        font-family: var(--Font-family-sans);
+        font-size: var(--Font-size-11);
+        font-weight: var(--Font-weight-regular);
+        line-height: var(--Line-height-140);
+    `,
+    Text12Regular: styled.span`
+        font-family: var(--Font-family-sans);
+        font-size: var(--Font-size-12);
+        font-weight: var(--Font-weight-regular);
+        line-height: var(--Line-height-140);
+    `,
+    Text13Regular: styled.span`
+        font-family: var(--Font-family-sans);
+        font-size: var(--Font-size-13);
+        font-weight: var(--Font-weight-regular);
+        line-height: var(--Line-height-140);
+    `,
+    Text14Regular: styled.span`
+        font-family: var(--Font-family-sans);
+        font-size: var(--Font-size-14);
+        font-weight: var(--Font-weight-regular);
+        line-height: var(--Line-height-140);
+    `,
+    Text15Regular: styled.span`
+        font-family: var(--Font-family-sans);
+        font-size: var(--Font-size-15);
+        font-weight: var(--Font-weight-regular);
+        line-height: var(--Line-height-140);
+    `,
+}
