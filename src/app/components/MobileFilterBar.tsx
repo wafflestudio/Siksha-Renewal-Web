2af216fbb42cb9df9ecee30386ac8bc4c722f508
임혜진 <hjlim7831@gmail.com>
2025-05-18 16:35:17 +0900

    chore: 미사용 svg 제거

--- src/app/components/MobileFilterBar.tsx
+++ src/app/components/MobileFilterBar.tsx
@@ -189,7 +189,7 @@ export default function MobileFilterBar() {
           <ButtonText isActive={isSet.category}>
             {isSet.category ? `${filterList.category.join(", ")}` : "카테고리"}
           </ButtonText>
-<StyledDownArrowIcon aria-label="아래 화살표" />
+          <StyledDownArrowIcon aria-label="아래 화살표" />
         </Button> */}
       </Container>
     </>
