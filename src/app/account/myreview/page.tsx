2f9ba73ee2d326fd7ce765f6c5b89bf4a649b9f2
B0neh3ad <js1044k@gmail.com>
2025-08-23 10:49:11 +0900

    Chore: add TODO comment

--- src/app/account/myreview/page.tsx
+++ src/app/account/myreview/page.tsx
@@ -138,6 +138,7 @@ const Container = styled.div`
   border-radius: 8px;
   box-sizing: border-box;
 
+  // TODO: account/layout.tsx 수정 후에 그에 맞춰서 수정해야 함.
   @media (max-width: 768px) {
     width: calc(100% + 32px);
     margin-top: -24px;
