349fe756ac50b68b28c44edf87941a0458d9fac6
B0neh3ad <js1044k@gmail.com>
2025-09-15 00:26:08 +0900

    fix: unnecessary import

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -19,10 +19,7 @@ import useError from "hooks/useError";
 import TwoColumnLayout from "styles/layouts/TwoColumnLayout";
 import MobileFilterBar from "./components/MobileFilterBar";
 import FestivalToggle from "./components/FestivalToggle";
-import { initMixpanel } from "utils/MixPanel";
 import useLikedMenuIntro from "hooks/UseLikedMenuIntro";
-import { getIsFestival } from "utils/api/festival";
-import { set } from "react-hook-form";
 
 export default function Home() {
   const state = useStateContext();
