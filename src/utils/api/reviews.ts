5d1ac84fcd7dfc878482aa53d63a0482d50354c1
임혜진 <hjlim7831@gmail.com>
2025-10-04 20:22:35 +0900

    fix: ReviewType 수정

--- src/utils/api/reviews.ts
+++ src/utils/api/reviews.ts
@@ -1,3 +1,4 @@
+import { ReviewType } from "app/menu/[menuId]/Menu";
 import axios from "axios";
 import APIendpoint from "constants/constants";
 import { KeywordReviewScore, RawReview } from "types";
@@ -7,7 +8,7 @@ export const getReviews = (
 ): Promise<{
   totalCount: number;
   hasNext: boolean;
-  result: RawReview[];
+  result: ReviewType[];
 }> => {
   return axios.get(`${APIendpoint()}/reviews?menu_id=${menuID}&page=1&per_page=100`).then((res) => {
     const {
