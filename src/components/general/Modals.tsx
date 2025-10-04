4380437fc62e1d68dab9c004f49748739f21953b
임혜진 <hjlim7831@gmail.com>
2025-10-04 11:24:18 +0900

    fix: 모달 안열리는 현상 수정

--- src/components/general/Modals.tsx
+++ src/components/general/Modals.tsx
@@ -14,7 +14,7 @@ export default function Modals() {
       const { Component } = openedModals[openedModals.length - 1];
       closeModal(Component);
     }
-  }, [closeModal, currentPath, openedModals]);
+  }, [currentPath]);
 
   return (
     <>
