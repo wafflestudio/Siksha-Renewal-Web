751b5e1f073fab34d63d5523edb0e23b6c4930b5
B0neh3ad <js1044k@gmail.com>
2025-10-05 15:59:33 +0900

    feat: add & apply stars component for myreview

---
+++ src/app/account/myreview/components/Stars.tsx
@@ -0,0 +1,31 @@
+import styled from "styled-components";
+
+export default function Stars({ score }: { score: number }) {
+  return (
+    <StarContainer>
+      {Array.from(Array(5).keys()).map((i) => {
+        if (score >= i + 1) {
+          return <Star src="/img/general/star-on-13.svg" key={i} alt="별점 채워짐" />;
+        } else if (score >= i + 0.5) {
+          return <Star src="/img/general/star-on-13.svg" key={i} alt="별점 채워짐" />;
+        } else {
+          return <Star src="/img/general/star-off-13.svg" key={i} alt="별점 비워짐" />;
+        }
+      })}
+    </StarContainer>
+  );
+}
+
+const StarContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  width: 61px;
+`;
+
+const Star = styled.img`
+  width: 13px;
+  height: 13px;
+  margin-right: -1px;
+  fill: #ffd600;
+`;
