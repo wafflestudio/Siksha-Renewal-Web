2fb67bf64cf918502ba0b3614e09bf983c6810fd
B0neh3ad <js1044k@gmail.com>
2025-11-03 19:12:41 +0900

    chore: check review.etc.images instead of review.etc & update key of photo in photoreviewssection

--- src/app/menu/[menuId]/components/PhotoReviewsSection.tsx
+++ src/app/menu/[menuId]/components/PhotoReviewsSection.tsx
@@ -44,8 +44,8 @@ export default function PhotoReviewsSection({
       </Header>
       <Photos>
         {images.length > 0 ? (
-          images.filter((image) => image && image.trim() !== "").map((image, index) => (
-            <ImageWrapper key={image + index}>
+          images.map((image, index) => (
+            <ImageWrapper key={index}>
               {isMobile && imagesCount > MAX_NUMBER_OF_PREVIEW_IMAGES && index === MAX_NUMBER_OF_PREVIEW_IMAGES - 1 && (
                 <Link href={`/menu/${menuId}/photos`}>
                   <MoreImages>{imagesCount - MAX_NUMBER_OF_PREVIEW_IMAGES}건 더보기</MoreImages>
