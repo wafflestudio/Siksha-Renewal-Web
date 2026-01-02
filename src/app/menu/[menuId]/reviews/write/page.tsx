2bb6db9079737fb4e01ffeae6e4123933b0f1ba8
임혜진 <hjlim7831@gmail.com>
2026-01-02 23:43:14 +0900

    feat: 리뷰 등록 모바일 뷰 QA 반영영

--- src/app/menu/[menuId]/reviews/write/page.tsx
+++ src/app/menu/[menuId]/reviews/write/page.tsx
@@ -156,11 +156,16 @@ export default function ReviewPost() {
           </StarsContainer>
           <Score>{inputs.score}</Score>
         </Header>
+        <Divider />
+        <FormContainer>
         <KeywordReviewForm inputs={inputs} setInputs={setInputs} />
         <CommentSection>
           <div style={{ display: "flex", alignItems: "center" }}>
             <StyledCommentReviewIcon />
-            <CommentTitle>식단 한 줄 평을 함께 남겨보세요!</CommentTitle>
+            <CommentTitle>식단 한 줄 평을 함께 남겨보세요!
+ <span>(선택)</span>
+
+            </CommentTitle>
           </div>
           <div style={{ position: "relative" }}>
             <CommentTextArea
@@ -220,6 +225,7 @@ export default function ReviewPost() {
             disabled={inputs.comment.length === 0}
           />
         </Footer>
+        </FormContainer>
       </Container>
     </>
   );
@@ -246,6 +252,8 @@ const Container = styled(OneColumnLayout.Container)`
     flex: 1;
     margin-top: 0px;
     margin-bottom: 0px;
+    padding-left: 0px;
+    padding-right: 0px;
   }
 `;
 
@@ -333,16 +341,7 @@ const SelectStarText = styled.span`
 
   @media (max-width: 768px) {
     display: inherit;
-  }
-`;
-
-const Star = styled.img`
-  width: 28px;
-  height: 28px;
-  cursor: pointer;
-  @media (max-width: 768px) {
-    width: 30px;
-    height: 30px;
+    margin-bottom: 10px;
   }
 `;
 
@@ -367,7 +366,8 @@ const StarsContainer = styled.div`
   cursor: pointer;
   @media (max-width: 768px) {
     width: 150px;
-    margin-bottom: 7px;
+    margin-top: 0px;
+    margin-bottom: 10px;
   }
 `;
 
@@ -386,7 +386,8 @@ const Score = styled.div`
   letter-spacing: var(--Font-letter-spacing-0, -0.3px);
 
   @media (max-width: 768px) {
-    margin-top: 7px;
+    margin-top: 0px;
+    margin-bottom: 20px;
     color: var(--Color-Foundation-base-black, #000);
     text-align: center;
 
@@ -396,6 +397,27 @@ const Score = styled.div`
     font-style: normal;
     font-weight: var(--Font-weight-bold, 700);
     line-height: 140%; /* 28px */
+  }
+`;
+
+const Divider = styled.div`
+  display: none;
+
+  @media (max-width: 768px) {
+    display: flex;
+    height: 10px;
+    width: 100%;
+    background-color: var(--Color-Foundation-gray-100);
+  }
+`;
+
+const FormContainer = styled.div`
+
+
+@media (max-width: 768px) {
+position: relative;
+    padding-left: 16px;
+    padding-right: 16px;
   }
 `;
 
@@ -436,8 +458,7 @@ const CommentTextArea = styled.textarea`
 const StyledCommentReviewIcon = styled(CommentReviewIcon)`
   color: var(--Color-Foundation-gray-700, #b7b7b7);
   @media (max-width: 768px) {
-    width: 18px;
-    height: 18px;
+  color: var(--Color-Foundation-base-black);
   }
 `;
 
@@ -453,8 +474,23 @@ const CommentTitle = styled.div`
 
   margin-left: 6px;
 
+    span {
+    letter-spacing: -0.3px;
+    font-weight: 700;
+    font-size: 14px;
+    line-height: 150%;
+    color: var(--Color-Foundation-gray-600);
+    margin-left: 4px;
+
+    @media (max-width: 768px) {
+      color: var(--Color-Foundation-gray-700);
+      font-size: 12px;
+    }
+  }
+
   @media (max-width: 768px) {
-    font-weight: var(--Font-weight-bold, 700);
+    color: var(--Color-Foundation-base-black);
+    font-size: 18px;
   }
 `;
 
@@ -650,7 +686,10 @@ const Footer = styled.div`
     position: absolute;
     display: inherit;
     bottom: 0;
-    padding: 0 24px 24px;
+    padding-bottom: 24px;
+    left: 16px;
+    right: 16px;
+    width: auto;
   }
 `;
 
